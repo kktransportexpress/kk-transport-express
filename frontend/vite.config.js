8949c84bfff8f783aa719ae881a06cc6da0c775e
Luis Puga <luispuga@Luiss-MacBook-Pro.local>
2025-10-15 22:30:10 -0400

    Fix vite base path for production

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: '/', // <-- Esto es lo correcto para Render con dominio propio
   build: {
     outDir: 'dist'
   }
